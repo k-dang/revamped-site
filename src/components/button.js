780f5cc832c0cc383a27888ad8390ba03e20c442
k-dang <dangkevin12@gmail.com>
2019-06-20 22:44:06 -0400

    add button component

---
+++ src/components/button.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+import buttonStyles from "./button.module.css"
+
+class Button extends React.Component {
+  clicked() {
+    console.log("clicked")
+  }
+  render() {
+    return (
+      <button className={buttonStyles.button} onClick={() => this.clicked()}>
+        {this.props.buttonText}
+      </button>
+    )
+  }
+}
+
+Button.propTypes = {
+  buttonText: PropTypes.string,
+}
+
+export default Button
